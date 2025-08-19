a067ed4aeff034b4819a0a092a6ec3bba914663f
Amol Pate <amol.pate1@Indussoft.com>
2025-08-19 18:19:01 +0530

    making war instead of jar

--- jenkin/src/main/java/com/ebixcash/jenkin/JenkinWebAppApplication.java
+++ jenkin/src/main/java/com/ebixcash/jenkin/JenkinWebAppApplication.java
@@ -2,12 +2,19 @@ package com.ebixcash.jenkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinWebAppApplication {
+public class JenkinWebAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinWebAppApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JenkinWebAppApplication.class);
+    }
 
 }
